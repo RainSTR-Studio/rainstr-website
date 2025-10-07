c6ab534182266bf6a8b5215e4858133e9282e829
zhangxuan2011 <zx20110412@outlook.com>
2025-10-07 13:38:00 +0800

    添加了跳转个人主页的按钮

--- src/client/components/GitHubCard.vue
+++ src/client/components/GitHubCard.vue
@@ -4,6 +4,9 @@
   const openGithub = (user, repo) => {
     window.open(`https://github.com/${user}/${repo}`, '_blank')
   }
+  const openHomepage = (user) => {
+    window.open(`https://www.rainstr-studio.top/members/${user}`, '_blank')
+  }
 </script>
 
 <template>
@@ -18,7 +21,13 @@
         </div>
       </div>
       <p class="universal-description">{{ description }}</p>
-      <el-button @click="openGithub(from, repo)"class="github-btn"type="primary" round>
+      <el-button @click="openHomepage(from)" class="github-btn" type="primary" round>
+        查看个人主页
+      </el-button>
+      <div style="margin-buttom: 5px">
+        <!-- 预留空间 (去掉会发生奇怪的事情) -->
+      </div>
+      <el-button @click="openGithub(from, repo)" class="github-btn" type="default" style="margin-top: 5px" round>
         查看 GitHub
       </el-button>
     </div>
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: ['www.rainstr-studio.top', 'rainstr-studio.top', 'localhost', '127.0.0.1']
+  },
   plugins: [vue()],
 });
